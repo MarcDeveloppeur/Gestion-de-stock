cd68c03b17394bedcba986c83092bfb9d8763b29
Marc <marcramadison006@gmail.com>
2021-02-07 17:25:32 +0300

    modifier la couleur de l'interface et ajouter le document unité dans la base

--- Back-end/Models/productModel.js
+++ Back-end/Models/productModel.js
@@ -2,6 +2,7 @@ const mongoose=require('mongoose');
 
 const ProductSchema=mongoose.Schema({
   nom:{type:String,required:true},
+  unite:{type:String,required:true},
   prix:{type:Number,required:true},
   nombre:{type:Number,required:true},
   estDispo:{type:Boolean,required:true}
--- Front-end/src/Pages/Enregistrement.js
+++ Front-end/src/Pages/Enregistrement.js
@@ -8,18 +8,20 @@ function Enregistrement() {
     const [prix,setPrix]=useState();
     const [nombre,setNombre]=useState();
     const [dispo,setDispo]=useState(false);
+    const [unite,setUnite]=useState('Pièce');
     const [message,setMessage]=useState('');
     const [messageClasse,setMessageClasse]=useState('');
     const history=useHistory();
 
     const enregistrer=(e)=>{
       e.preventDefault();
-      if(nom!==''||prix!==''||nombre!==''){
+      if(nom!==''||prix!==''||nombre!==''||unite!==''){
         const data={
           nom:nom,
           prix:prix,
           nombre:nombre,
-          estDispo:dispo
+          estDispo:dispo,
+          unite:unite
         }
         axios.post('http://localhost:5000/product/addNew',data)
         .then(()=>{
@@ -47,9 +49,23 @@ function Enregistrement() {
         <form onSubmit={enregistrer}>
            <label htmlFor="nom"><p>Nom du produit:</p></label>
            <input type="text" id="nom"  className="inputClass" value={nom} onChange={(e)=>setNom(e.target.value)}/><br/>
+           <label htmlFor="unite"><p>Unité:</p></label>
+           <select onChange={(e)=>setUnite(e.target.value)} id="unite" className="inputClass">
+                 <optGroup label="Pièce">
+                     <option value="Pièce">Pièce</option>
+                </optGroup>
+                <optGroup label="Poids">
+                     <option value="Tonne">Tonne</option>
+                     <option value="Kilogramme">Kilogramme</option>
+                     <option value="Gramme">Gramme</option>
+                </optGroup>
+                <optGroup label="Liquide">
+                     <option value="Littre">Littre</option>
+                </optGroup>
+          </select>
            <label htmlFor="prix"><p>Prix unitair (Ariary):</p></label>
            <input type="number" id="prix"  min={0} className="inputClass" value={prix} onChange={(e)=>setPrix(e.target.value)}/><br/>
-            <label htmlFor="nombre"><p>Nombre:</p></label>
+            <label htmlFor="nombre"><p>Nombre d'unité:</p></label>
            <input type="number" id="nombre" min={0} className="inputClass" value={nombre} onChange={(e)=>setNombre(e.target.value)}/><br/>
            <p>Disponible:</p>
            <label htmlFor="Oui">OUI
